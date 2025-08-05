4250f7a1ed714ae5fb9f8edd0e5b32e48aa4baec
guesswhojustme <imnayeonsaranghaee@gmail.com>
2025-08-05 17:38:29 +0800

    feat: add basic math operator functions

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log('hello world');
+function add(a, b){
+    return a + b;
+};
+
+function subracts(a, b){
+    return a - b;
+};
+
+function multiply(a, b){
+    return a * b;
+};
+
+function divide(a, b){
+    return a / b;
+};
+
+console.log(add(5,5));
+console.log(subracts(5,5));
+console.log(multiply(5,5));
+console.log(divide(5,5));
+
+
+
+
+
