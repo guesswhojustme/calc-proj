f0f8bf39bd02ec22796204bd287a561634428cb9
guesswhojustme <imnayeonsaranghaee@gmail.com>
2025-08-06 12:22:43 +0800

    feat: add calculation functionality
    The operation buttons now works, but still prone to bugs and errors

--- script.js
+++ script.js
@@ -1,41 +1,104 @@
 const input = document.querySelector('#input');
 const buttons = document.querySelectorAll('.btn');
-const clear = document.querySelector('#equal-btn');
+const clearBtn = document.querySelector('#clear-btn');
+const divideBtn = document.querySelector('#divide-btn')
+const multiplyBtn = document.querySelector('#multiply-btn')
+const subractBtn = document.querySelector('#subtract-btn')
+const addBtn = document.querySelector('#add-btn')
+const equalBtn = document.querySelector('#equal-btn')
+let change = document.querySelector('.change')
 
-clear.addEventListener('click', () => {
+clearBtn.addEventListener('click', () => {
+    console.log("clear clicked");
+    firstVal = 0;
+    secondVal = 0;
+    operator = '';
+    input.value = 0;
     
-})
+});
+
+divideBtn.addEventListener('click', () => {
+   change.textContent = '/' 
+   operator = '/'
+   firstVal = inputVal; 
+});
+
+subractBtn.addEventListener('click', () => {
+    change.textContent = '-'
+    operator = '-';
+    firstVal = inputVal;
+});
+
+addBtn.addEventListener('click', () => {
+    change.textContent = '+'
+    operator = '+';
+    firstVal = inputVal;
+    // input.value = 0;
+    console.log(firstVal);
+    
+});
+
+multiplyBtn.addEventListener('click', () => {
+    change.textContent = '*'
+    operator = '*';
+    firstVal = inputVal;
+});
+
+equalBtn.addEventListener('click', () => {
+    secondVal = inputVal;
+    console.log(secondVal);
+    console.log(operate(operator));
+});
 
 buttons.forEach(button => {
-    button.addEventListener('click', function() {
-        input.value += button.textContent;
+    button.addEventListener('click', () => {
+    if(input.value == 0){
+        input.value = '';
+        inputVal = '';
+    }
+    inputVal += button.textContent;
+    input.value += button.textContent
     });
 })
 
+let inputVal = 0;
+input.value = inputVal;
+let operator = '';
+
+let secondVal = 0;
+let firstVal = 0;
+
 function add(a, b){
-    return a + b;
+    return input.value =  a + b;
 };
 
 function subracts(a, b){
-    return a - b;
+    return input.value =  a - b;
 };
 
 function multiply(a, b){
-    return a * b;
+    return input.value =  a * b;
 };
 
 function divide(a, b){
-    return a / b;
+    return input.value =  a / b;
 };
 
-function operate(num1, operator, num2){
-    add()
-    subracts()
-    multiply()
-    divide()
+function operate(operator){
+   switch(operator){
+    case '+':
+        return add(parseInt(firstVal), parseInt(secondVal))
+        break;
+    case '*':
+        return multiply(parseInt(firstVal), parseInt(secondVal))
+        break;
+    case '/':
+        return divide(parseInt(firstVal), parseInt(secondVal))
+        break;
+    case '-':
+        return subracts(parseInt(firstVal), parseInt(secondVal))
+        break;
+   }
+
 }
-
-
-
-
 
