e1b6e1b627afa7cdcd963244e5effc38a2f8d1d2
guesswhojustme <imnayeonsaranghaee@gmail.com>
2025-08-07 15:48:43 +0800

    feat & refactor: improve console msg & code logic, remove redundant code
    I edited the code logic where its possible to nest a calculation and repeat a calculation
    when equal is clicked multiple time. Removed some redundant code and Improved the console log msgs
    for better readability and debugging

--- script.js
+++ script.js
@@ -12,109 +12,194 @@ clearBtn.addEventListener('click', () => {
     console.log("clear clicked");
     firstVal = '';
     secondVal = '';
+    firstInputVal = '';
+    secondInputVal = '';
     operator = '';
-    input.value = '';
     change.textContent = '';
+    firstInput = true;
+    secondInput = false;
+    input.value = '';
+    console.log(`first value: ${firstVal}` );
+    console.log(`second value: ${secondVal}` );
+    // console.log(`current first input value: ${firstInputVal}`);
+    // console.log(`current second input value: ${secondInputVal}`);
+    console.log(`first input active: ${firstInput}`);
+    console.log(`second input active: ${secondInput}`);
+    console.log('_____________________________________________');
 });
 
 divideBtn.addEventListener('click', () => {
+    if(firstVal !== '' && secondVal !== ''){
+        console.log(`${firstVal} ${operator} ${secondVal} = ${operate(operator)}`);
+        secondVal = '';
+    }
    change.textContent = '/' 
    operator = '/'
-   firstVal = inputVal; 
+   secondInput = true;
+   firstInput = false;
+//    firstVal = firstInputVal;
+
    input.value = '';
+   console.log("divide clicked");
    console.log(`current input value: ${inputVal}`);
    console.log(`first value: ${firstVal}` );
    console.log(`second value: ${secondVal}` );
-   console.log("divide clicked");
+   console.log(`first input active: ${firstInput}`);
+   console.log(`second input active: ${secondInput}`);
+   console.log('_____________________________________________');
 });
 
 subtractBtn.addEventListener('click', () => {
+    if(firstVal !== '' && secondVal !== ''){
+        console.log(`${firstVal} ${operator} ${secondVal} = ${operate(operator)}`);
+        secondVal = '';
+    }
+    console.log(`display active: ${displayVal}`);
     change.textContent = '-'
     operator = '-';
-    firstVal = inputVal;
+    secondInput = true;
+    firstInput = false;
+    // firstVal = firstInputVal;
     input.value = '';
+    console.log("subtract clicked");
     console.log(`current input value: ${inputVal}`);
     console.log(`first value: ${firstVal}` );
     console.log(`second value: ${secondVal}` );
-    console.log("subtract clicked");
+    console.log(`first input active: ${firstInput}`);
+    console.log(`second input active: ${secondInput}`);
+    console.log('_____________________________________________');
 });
 
 addBtn.addEventListener('click', () => {
+    if(firstVal !== '' && secondVal !== ''){
+        console.log(`${firstVal} ${operator} ${secondVal} = ${operate(operator)}`);
+        secondVal = '';
+    }
     change.textContent = '+'
     operator = '+';
-    firstVal = inputVal;
+    secondInput = true;
+    firstInput = false;
+    // firstVal = firstInputVal;
     input.value = '';
+    console.log("add clicked");
     console.log(`current input value: ${inputVal}`);
     console.log(`first value: ${firstVal}` );
     console.log(`second value: ${secondVal}` );
-    console.log("add clicked");
-   
+    console.log(`first input active: ${firstInput}`);
+    console.log(`second input active: ${secondInput}`);
+    console.log('_____________________________________________');
 });
 
 multiplyBtn.addEventListener('click', () => {
+    if(firstVal !== '' && secondVal !== ''){
+        console.log(`${firstVal} ${operator} ${secondVal} = ${operate(operator)}`);
+        secondVal = '';
+    }
     change.textContent = '*'
     operator = '*';
-    firstVal = inputVal;
+    secondInput = true;
+    firstInput = false;
+    // firstVal = firstInputVal;
     input.value = '';
+    console.log("multiply clicked");
     console.log(`current input value: ${inputVal}`);
     console.log(`first value: ${firstVal}` );
     console.log(`second value: ${secondVal}` );
-    console.log("multiply clicked");
+    console.log(`first input active: ${firstInput}`);
+    console.log(`second input active: ${secondInput}`);
+    console.log('_____________________________________________');
 });
 
 equalBtn.addEventListener('click', () => {
-    secondVal = inputVal;
+    // secondVal = secondInputVal;
+    change.textContent = '='
+    if(secondVal == ''){
+        displayVal = false;
+    }else{
+        displayVal = true;
+    }
     console.log("equal clicked");
     console.log(`first value: ${firstVal}`);
     console.log(`second value: ${secondVal}`);
     console.log(`${firstVal} ${operator} ${secondVal} = ${operate(operator)}`);
     console.log(`current input value: ${inputVal}`);
-    displayVal = true;
+    firstInput = true;
+    secondInput = false;
+    console.log(`first input active: ${firstInput}`);
+    console.log(`second input active: ${secondInput}`);
     console.log(`display active: ${displayVal}`);
+    console.log('_____________________________________________');
 });
 
 buttons.forEach(button => {
     button.addEventListener('click', () => {
     console.log(`display active: ${displayVal}`);
+    console.log(`first input active: ${firstInput}`);
+    console.log(`second input active: ${secondInput}`);
+    
     if(input.value == 0){
         inputVal = '';
     }
     if(displayVal == true){
         input.value = '';
         inputVal = '';
+        firstVal = '';
+        secondVal = '';
         displayVal = false;
     }
+    if(firstInput == true){
+        // firstInputVal += button.textContent
+        firstVal += button.textContent;
+    }
+    if(secondInput == true) {
+        // secondInputVal += button.textContent
+        secondVal += button.textContent;
+    }
+    
     inputVal += button.textContent;
     input.value += button.textContent;
     console.log(`current input value: ${inputVal}`);
+    // console.log(`current first input value: ${firstInputVal}`);
+    // console.log(`current second input value: ${secondInputVal}`);
+    console.log(`first value: ${firstVal}`);
+    console.log(`second value: ${secondVal}`);
+    console.log('_____________________________________________');
+    
     });
 })
+
+let secondInput = false
+let secondInputVal = '';
+
+let firstInput = true;
+let firstInputVal = ''
+
+let displayVal = false;
 
 let inputVal = '';
 let operator = '';
 
-let displayVal = false;
 let secondVal = '';
 let firstVal = '';
 
 function add(a, b){
-    inputVal = a + b
-    return input.value = inputVal
+    firstVal = a + b
+    return input.value = firstVal
 };
 
 function subracts(a, b){
-    inputVal = a - b
-    return input.value =  inputVal;
+    firstVal = a - b
+    return input.value =  firstVal;
 };
 
 function multiply(a, b){
-    inputVal = a * b
-    return input.value =  inputVal;
+    firstVal = a * b
+    return input.value =  firstVal;
 };
 
 function divide(a, b){
-    inputVal = a / b
-    return input.value = inputVal;
+    firstVal = a / b
+    return input.value = firstVal;
 };
 
 function operate(operator){
