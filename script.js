085ca425f346ed1cc4b1b3741ec4148aa71dfcee
guesswhojustme <imnayeonsaranghaee@gmail.com>
2025-08-11 21:47:33 +0800

    refactor: clean code
    Added some comments to some lines of code to understand its behaviors.
    And also utilized JS event delegation for better performance

--- script.js
+++ script.js
@@ -6,107 +6,101 @@ const multiplyBtn = document.querySelector('#multiply-btn')
 const subtractBtn = document.querySelector('#subtract-btn')
 const addBtn = document.querySelector('#add-btn')
 const equalBtn = document.querySelector('#equal-btn')
+const parentBtnsDiv = document.querySelector('.btn-div')
 let change = document.querySelector('.change')
 
+//States
 let firstInputActive = true;
 let secondInputActive = false
 let outputIsDisplaying = false;
 let isOperating = false;
 
+//Default values
 let secondVal = '';
 let firstVal = '';
 let totalVal = '';
 let operator = '';
 
-clearBtn.addEventListener('click', () => {
-    console.log("clear clicked");
-    firstVal = '';
-    secondVal = '';
-    totalVal = '';
-    operator = '';
-    change.textContent = '';
-    firstInputActive = true;
-    secondInputActive = false;
-    outputIsDisplaying = false;
-    isOperating = false;
-    input.value = '';
-    logMessages()
-});
+//Operation button functionalities
+parentBtnsDiv.addEventListener('click', (event) => {
+    let target = event.target
 
-divideBtn.addEventListener('click', () => {
-   checkSecondVal();
-   chainOperations();
-   secondVal = '';
-   change.textContent = '/' 
-   operator = '/'
-   outputIsDisplaying = false;
-   secondInputActive = true;
-   firstInputActive = false;
-   isOperating = true;
-   console.log("divide clicked");
-   logMessages();
-});
-
-subtractBtn.addEventListener('click', () => {
-    checkSecondVal();
-    chainOperations();
-    secondVal = '';
-    change.textContent = '-'
-    operator = '-';
-    outputIsDisplaying = false;
-    secondInputActive = true;
-    firstInputActive = false;
-    isOperating = true;
-    console.log("subtract clicked");
-    logMessages();
-});
-
-addBtn.addEventListener('click', () => {
-    checkSecondVal();
-    chainOperations();
-    secondVal = '';
-    change.textContent = '+'
-    operator = '+';
-    outputIsDisplaying = false;
-    secondInputActive = true;
-    firstInputActive = false;
-    isOperating = true;
-    console.log("add clicked");
-    logMessages();
-});
-
-multiplyBtn.addEventListener('click', () => {
-    checkSecondVal();
-    chainOperations();
-    secondVal = '';
-    change.textContent = '*'
-    operator = '*';
-    outputIsDisplaying = false;
-    secondInputActive = true;
-    firstInputActive = false;
-    isOperating = true;
-    console.log("multiply clicked");
-    logMessages();
-});
-
-equalBtn.addEventListener('click', () => {
-    if(!operator){
-        return 
-    }else if(secondVal === ''){
-        secondVal = firstVal;
+    switch (target.id) {
+        case 'divide-btn':
+                resetOperatorValue();
+                chainOperations();
+                secondVal = '';
+                change.textContent = '/' 
+                operator = '/'
+                changeStatesStatus();
+                console.log("divide clicked");
+                logMessages();
+            break;
+        case 'multiply-btn':
+                resetOperatorValue();
+                chainOperations();
+                secondVal = '';
+                change.textContent = '*'
+                operator = '*';
+                changeStatesStatus();
+                console.log("multiply clicked");
+                logMessages();
+            break;
+        case 'subtract-btn':
+                resetOperatorValue();
+                chainOperations();
+                secondVal = '';
+                change.textContent = '-'
+                operator = '-';
+                changeStatesStatus();
+                console.log("subtract clicked");
+                logMessages();
+            break;
+        case 'add-btn':
+                resetOperatorValue();
+                chainOperations();
+                secondVal = '';
+                change.textContent = '+'
+                operator = '+';
+                changeStatesStatus();
+                console.log("add clicked");
+                logMessages();
+            break;
+        case 'clear-btn':
+                console.log("clear clicked");
+                firstVal = '';
+                secondVal = '';
+                totalVal = '';
+                operator = '';
+                change.textContent = '';
+                firstInputActive = true;
+                secondInputActive = false;
+                outputIsDisplaying = false;
+                isOperating = false;
+                input.value = '';
+                logMessages()
+            break;
+        case 'equal-btn':
+                if(!operator){
+                    return 
+                }else if(secondVal === ''){
+                    secondVal = firstVal;
+                }
+                else{
+                    outputIsDisplaying  = true;
+                }
+                change.textContent = '='
+                firstInputActive = true;
+                secondInputActive = false;
+                console.log('equal clicked');
+                console.log(`${firstVal} ${operator} ${secondVal} = ${operate(operator)}`);
+                firstVal = totalVal;
+                logMessages()
+            break;
     }
-    else{
-        outputIsDisplaying  = true;
-    }
-    change.textContent = '='
-    firstInputActive = true;
-    secondInputActive = false;
-    console.log('equal clicked');
-    console.log(`${firstVal} ${operator} ${secondVal} = ${operate(operator)}`);
-    firstVal = totalVal;
-    logMessages()
-});
+})
 
+//Number input buttons, add numbers in input
 buttons.forEach(button => {
     button.addEventListener('click', () => {
     if(outputIsDisplaying){
@@ -130,6 +124,7 @@ buttons.forEach(button => {
     });
 })
 
+//For chaining an operator, runs if user is trying trying to chain an operator
 function chainOperations(){
     if(operator && outputIsDisplaying == false){ 
     operate(operator)
@@ -140,12 +135,20 @@ function chainOperations(){
     }
 }
 
-function checkSecondVal() {
+//Runs if second value is not present and it resets operator value to allow user change operator when the first value is already defined
+function resetOperatorValue() {
     if(secondVal === '') {
         operator = '';
     }
 }
 
+//Change states status when operating
+function changeStatesStatus(){
+    outputIsDisplaying = false;
+    secondInputActive = true;
+    firstInputActive = false;
+    isOperating = true;
+}
 
 function logMessages(){
     console.log(`displaying value? : ${outputIsDisplaying }`);
